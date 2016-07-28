307d52556e667b80937a75460a04f1e4d74c8000
Russell Pruitt <pruitt.russell@gmail.com>
2016-07-28 16:06:53 -0400

    adjust app.run to accommodate for heroku port

--- app.py
+++ app.py
@@ -96,4 +96,6 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
